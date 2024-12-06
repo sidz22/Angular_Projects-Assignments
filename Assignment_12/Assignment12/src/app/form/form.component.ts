641b75656ca28ac3444b1abfd801774626b65b1b
sidz22 <siddharthdalbhanjan22@gmail.com>
2024-12-06 18:18:19 +0530

    Added Assignment 12 with the all Modules

---
+++ Assignment_12/Assignment12/src/app/form/form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent 
+{
+  onSubmit()
+  {
+    alert('Form submitted Successfully');
+  }
+}
